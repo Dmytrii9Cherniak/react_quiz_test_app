920fc4d05427b8e8b88e58ea9929f58b2c1efa81
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-06-22 23:05:59 +0300

    added results page and some styles

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import '../results/Results.scss';
 
 function Results(props) {
     return (
-        <div></div>
+        <div className="resultsPage">
+
+            <div>
+                <h4> Results page -  quiz </h4>
+            </div>
+
+            <ul className="list-group">
+                <li className="list-group-item"> Total quiz played -  </li>
+                <li className="list-group-item"> Total incorrect answers -  </li>
+                <li className="list-group-item"> Total correct answers -  </li>
+            </ul>
+
+            <button className="btn btn-primary"> Back to quiz list </button>
+
+        </div>
     );
 }
 
