a2d55a542c3184a45b0f76e3c33203c83f824b35
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-06-24 22:28:36 +0300

    code refactorring

--- src/hoc/quizProvider.js
+++ src/hoc/quizProvider.js
@@ -1,5 +1,5 @@
 import React, {createContext, useEffect, useState} from 'react';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 export const QuizContext = createContext();
 
